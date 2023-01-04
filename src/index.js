34c7363e632e10fbc8cd24756ccd672428851a75
Koras02 <wjdgh0727@gmail.com>
2023-01-04 09:08:58 +0900

    Nodemon use

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 1. 헬로 솔저 서버에 console 과 body 텍스트 띄우기
-// app.use(ctx => {
-//     ctx.body = "Hello Koa nice me to!!";
-// });
+app.use(ctx => {
+    ctx.body = "Hellso sKoa nice me to!!";
+});
 
 // app.listen(4000, () => {
 //     console.log("koa server is listent to port 4000");
@@ -58,24 +58,30 @@ const app = new Koa();
 // })
 
 // 4. async/await 사용
-app.use(async (ctx, next) => {
-    console.log(1);
-    const started = new Date();
-    await next();
-    console.log(new Data() - started - 'ms');
-});
+// app.use(async (ctx, next) => {
+//     console.log(1);
+//     const started = new Date();
+//     await next();
+//     console.log(new Data() - started - 'ms');
+// });
 
-app.use((ctx, next) => {
-    console.log(2);
-    // 다음 문장들을 넣기 위해 next()를 띄워준다.
-    next();
-});
+// app.use((ctx, next) => {
+//     console.log(2);
+//     ctx.body = "Hello Kosa nice me to!!";
 
-app.use((ctx, next) => {
-    ctx.body = "hello Koa";
-    next();
-});
+//     // 다음 문장들을 넣기 위해 next()를 띄워준다.
+//     next();
+    
+// });
+
+// app.use((ctx, next) => {
+//     ctx.body = "hello Koa";
+//     next();
+// });
 
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
 })
+
+
+// 3. koa-router 사용하기
